711e261f89627b54e1a2b03a893d35aebba92529
19010651 <19010651@DESKTOP-4KUNCB9>
2020-08-04 14:00:13 +0800

    created a variable for assetTag in loanChromebook

--- ResourceCentre_P11/src/ResourceCentre.java
+++ ResourceCentre_P11/src/ResourceCentre.java
@@ -243,7 +243,8 @@ public class ResourceCentre {
 		boolean isLoaned = false;
 
 		for (int i = 0; i < chromebookList.size(); i++) {
-			if (tag.equalsIgnoreCase(chromebookList.get(i).getAssetTag())
+			String assetTag = chromebookList.get(i).getAssetTag();
+			if (tag.equalsIgnoreCase(assetTag)
 					&& chromebookList.get(i).getIsAvailable() == true) {
 
 				chromebookList.get(i).setIsAvailable(false);

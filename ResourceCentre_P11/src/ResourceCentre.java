d2c88b270d69b3f3303cbbe43b53f22269e39648
19045168 <19045168@NSNP-TH0NK>
2020-08-04 11:38:40 +0800

    commit message goes here

--- ResourceCentre_P11/src/ResourceCentre.java
+++ ResourceCentre_P11/src/ResourceCentre.java
@@ -257,7 +257,6 @@ public class ResourceCentre {
 	}
 
 	public static void loanChromebook(ArrayList<Chromebook> chromebookList) {
-		// write your code here
 		ResourceCentre.viewAllChromebook(chromebookList);
 		String tag = Helper.readString("Enter asset tag > ");
 		String due = Helper.readString("Enter due date > ");
